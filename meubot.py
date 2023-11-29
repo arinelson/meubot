c426e989e70a272b1a4a80d9e42888237a2fd1e1
Arinelson Santos <48639869+arinelson@users.noreply.github.com>
2023-11-28 23:26:14 -0300

    Update meubot.py

--- meubot.py
+++ meubot.py
@@ -1,5 +1,5 @@
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from datetime import datetime
 import pytz
 from cachetools import TTLCache
@@ -9,54 +9,46 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 # Variáveis globais
-TOKEN = "6942272197:AAE8kJKRkz_y3CbOgGzXl_ocVlnrvG51MM0"
+TOKEN = "6942272197:AAG_caCl5wdbYAHbu2tf1yUP7lXNSslVuwk"
+
+# Informações específicas de localização
+localizacao = {
+    "PAIS": "Brasil",
+    "ESTADO": "Alagoas",
+    "MUNICIPIO": "Maceió",
+}
 
 # Criação do cache com uma capacidade de 1 item e tempo de vida de 60 segundos
-cache = TTLCache(maxsize=1, ttl=30)
+cache = TTLCache(maxsize=1, ttl=60)
 
 # Função para enviar mensagens
-def enviar_mensagem(chat_id, mensagem, context):
-    try:
-        context.bot.send_message(chat_id, mensagem)
-    except Exception as e:
-        logger.error(f"Erro ao enviar mensagem: {e}")
+def enviar_mensagem(chat_id, mensagem):
+    context.bot.send_message(chat_id, mensagem)
 
 # Função para exibir a hora e o fuso horário atual
 def handle_horario(update, context):
     chat_id = update.effective_chat.id
     name = update.effective_user.first_name
 
-    # Limpar cache antes de obter a resposta
-    cache.clear()
-
     # Verifica se a resposta está em cache
     if "horario" in cache:
         response = cache["horario"]
     else:
-        try:
-            # Obtém a hora e o fuso horário atual
-            now = datetime.now()
-            user_location = update.effective_user.location
-            tz = pytz.timezone(user_location.timezone) if user_location else pytz.timezone('America/Maceio')
-            hora_atual = now.astimezone(tz).strftime("%H:%M:%S")
-            periodo_dia = get_periodo_dia(now.hour)
+        # Obtém a hora e o fuso horário atual
+        now = datetime.now()
+        user_location = update.effective_user.location
+        tz = pytz.timezone(user_location.timezone) if user_location else pytz.timezone('America/Maceio')
+        hora_atual = now.astimezone(tz).strftime("%H:%M:%S")
+        periodo_dia = get_periodo_dia(now.hour)
 
-            # Obtém a localização do usuário
-            pais = user_location.country_code if user_location else "Brasil"
-            estado = user_location.state if user_location else "Alagoas"
-            municipio = user_location.city if user_location else "Maceió"
+        # Monta a resposta
+        response = f"{name}, agora são {hora_atual} {periodo_dia} do {localizacao['PAIS']}, {localizacao['ESTADO']}, {localizacao['MUNICIPIO']}."
 
-            # Monta a resposta
-            response = f"{name}, agora são {hora_atual} {periodo_dia} do {pais}, {estado}, {municipio}."
-
-            # Armazena a resposta no cache
-            cache["horario"] = response
-        except Exception as e:
-            logger.error(f"Erro ao processar o horário: {e}")
-            response = "Desculpe, ocorreu um erro ao obter o horário."
+        # Armazena a resposta no cache
+        cache["horario"] = response
 
     # Envia a mensagem com a hora e o fuso horário
-    enviar_mensagem(chat_id, response, context)
+    enviar_mensagem(chat_id, response)
 
 # Funções de saudação e ajuda
 def handle_greeting(update, context):
@@ -71,44 +63,41 @@ def handle_greeting(update, context):
         "/ajuda - Tô ferrado(a)",
         "/contato - Quero falar com o boss",
         "/horario - Tô perdido na hora",
-        "/outrocomando - Este é outro comando!",
     ]
 
     # Envia as mensagens
-    enviar_mensagem(chat_id, saudacao, context)
+    enviar_mensagem(chat_id, saudacao)
     for opcao in opcoes:
-        enviar_mensagem(chat_id, opcao, context)
+        enviar_mensagem(chat_id, opcao)
 
 # Função de ajuda
-def handle_ajuda(update, context):
+def handle_help(update, context):
     chat_id = update.effective_chat.id
-    enviar_mensagem(chat_id, "Precisa de uma ajudinha? Estou aqui para ajudar!")
+    enviar_mensagem(chat_id, "Então você quer uma ajudinha, não é mesmo?")
 
 # Função de contato
-def handle_contato(update, context):
+def handle_contact(update, context):
     chat_id = update.effective_chat.id
-    enviar_mensagem(chat_id, "Quer falar com o chefe? Tente lá: @arinelson", context)
+    enviar_mensagem(chat_id, "Poxa, tente lá e veja se o chefe te responde: @arinelson")
 
-# Função para outro comando
-def handle_outro_comando(update, context):
-    chat_id = update.effective_chat.id
-    enviar_mensagem(chat_id, "Este é outro comando!")
-
-# Manipulador para mensagens de texto não relacionadas a comandos
-def handle_mensagem(update, context):
-    chat_id = update.effective_chat.id
-    enviar_mensagem(chat_id, "Não entendi essa mensagem.", context)
+# Função para obter o período do dia
+def get_periodo_dia(hour):
+    if 5 <= hour < 12:
+        return "da manhã"
+    elif 12 <= hour < 18:
+        return "da tarde"
+    else:
+        return "da noite"
 
 # Inicia o bot
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 # Adiciona os handlers
+dispatcher.add_handler(CommandHandler("ajuda", handle_help))
+dispatcher.add_handler(CommandHandler("contato", handle_contact))
 dispatcher.add_handler(CommandHandler("horario", handle_horario))
-dispatcher.add_handler(CommandHandler("ajuda", handle_ajuda))
-dispatcher.add_handler(CommandHandler("contato", handle_contato))
-dispatcher.add_handler(CommandHandler("outrocomando", handle_outro_comando))
-dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_mensagem))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_greeting))
 
 # Inicia o polling
 updater.start_polling()
